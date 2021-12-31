5900927adc13284a7af109fbb351a838be1fbab8
sam <samslusky@gmail.com>
2021-12-30 20:51:51 -0500

    It Starts
    Have the app so it loads, now need to completely change everything I guess

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,22 @@ export const environment = {
   production: false,
   
   firebaseConfig :{
- apiKey: "",
- authDomain: "",
- databaseURL: "",
- projectId: "",
- storageBucket: "",
- messagingSenderId: "",
- appId: "",
- measurementId: ""
+    apiKey: "AIzaSyBLrSIrkwnJJaVSNGY8niPI3t1D6EVPqx0",
+
+    authDomain: "lolstatcheck.firebaseapp.com",
+  
+    databaseURL: "https://lolstatcheck.firebaseio.com",
+  
+    projectId: "lolstatcheck",
+  
+    storageBucket: "lolstatcheck.appspot.com",
+  
+    messagingSenderId: "136402630844",
+  
+    appId: "1:136402630844:web:cd76f3e3c9554aceae38b9",
+  
+    measurementId: "G-E5BL4QJK2M"
+  
 
 }
 }
